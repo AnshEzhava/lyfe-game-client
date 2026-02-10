d410fec799b8ed9a224669fdec76b61a6972a00a
Ansh <119027857+AnshEzhava@users.noreply.github.com>
2026-02-10 11:21:40 +0530

    feat: game related pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Game } from './pages/game/game';
+import { Auth } from './pages/game/subpages/auth/auth';
+import { Play } from './pages/game/subpages/play/play';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
-  { path: 'game', component: Game },
+  {
+    path: 'game',
+    component: Game,
+    children: [
+      { path: 'auth', component: Auth },
+      { path: 'play', component: Play },
+    ],
+  },
   { path: '**', redirectTo: '' },
 ];
--- src/app/pages/game/game.ts
+++ src/app/pages/game/game.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
-export class Game {
-
-}
+export class Game {}
